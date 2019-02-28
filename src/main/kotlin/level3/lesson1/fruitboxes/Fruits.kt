f6632802b824898f5af55148e18cd4e00d4820f3
Dister <disterko@gmail.com>
2019-02-28 21:16:43 +0300

    task3 small fix3

--- src/main/kotlin/level3/lesson1/fruitboxes/Fruits.kt
+++ src/main/kotlin/level3/lesson1/fruitboxes/Fruits.kt
@@ -2,6 +2,6 @@ package level3.lesson1.fruitboxes
 
 sealed class Fruit(val weight: Double)
 
-class Apple() : Fruit(1.0)
+class Apple : Fruit(1.0)
 
-class Orange() : Fruit(1.5)
+class Orange : Fruit(1.5)
