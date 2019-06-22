0b15b5b6974d91d1c886cf4c9850fd1f070891bf
Dister <disterko@gmail.com>
2019-06-22 14:12:04 +0300

    add algorithms.lesson1

---
+++ src/main/kotlin/algorithms/lesson1/ArrayImpl.kt
@@ -0,0 +1,87 @@
+package algorithms.lesson1
+
+
+class ArrayImpl : Array {
+    private var array = IntArray(INITIAL_ARRAY_SIZE)
+
+    private var size = 0
+    override fun add(value: Int): Boolean {
+        if (isFull()) {
+            checkGrow()
+        }
+        array[size++] = value
+        return true
+    }
+
+    override fun checkGrow() {
+        array = array.copyOf(array.size + (array.size / 2))
+    }
+
+    override fun remove(value: Int): Boolean {
+        val index = indexOf(value)
+        return if (index != -1) {
+            removeByIndex(index)
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun removeByIndex(index: Int): Boolean {
+        if (index < 0 || index >= size) {
+            return false
+        }
+
+        for (i in index until size) {
+            array[i] = array[i + 1]
+        }
+        size--
+        return true
+    }
+
+    override fun find(value: Int): Boolean {
+        return indexOf(value) != -1
+    }
+
+    override fun indexOf(value: Int): Int {
+        for (i in 0 until size) {
+            if (array[i] == value) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    override fun display() {
+        println(this)
+    }
+
+    override fun copy(): IntArray {
+        return array.copyOf()
+    }
+
+    override fun isEmpty(): Boolean {
+        return size == 0
+    }
+
+    override fun isFull(): Boolean {
+        return size == array.size
+    }
+
+    override fun getSize(): Int {
+        return size
+    }
+
+    override fun toString(): String {
+        return array.joinToString(
+            separator = ", ",
+            prefix = "[",
+            postfix = "]"
+        ) { it.toString() }
+    }
+
+    companion object {
+        private const val INITIAL_ARRAY_SIZE = 10
+    }
+}
+
---
+++ src/main/kotlin/algorithms/lesson1/ArrayTest.kt
@@ -0,0 +1,30 @@
+package algorithms.lesson1
+
+import kotlin.random.Random.Default.nextInt
+import kotlin.system.measureTimeMillis
+
+private const val ARRAY_TEST_SIZE = 10_000
+
+fun main() {
+    val arr: Array = ArrayImpl()
+
+    for (i in 0 until ARRAY_TEST_SIZE) {
+        arr.add(nextInt(ARRAY_TEST_SIZE) + 1)
+    }
+
+    val arr1 = arr.copy()
+    val arr2 = arr.copy()
+    val arr3 = arr.copy()
+
+    println("Bubble sort: " + measureTimeMillis {
+        ArrayUtils.sortBubble(arr1)
+    })
+
+    println("Selection sort: " + measureTimeMillis {
+        ArrayUtils.sortBubble(arr2)
+    })
+
+    println("Insertion sort: " + measureTimeMillis {
+        ArrayUtils.sortBubble(arr3)
+    })
+}
